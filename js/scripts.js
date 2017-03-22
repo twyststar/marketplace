fbbe442ae7a536216536bd6d590a48e6e8e533ee
Starlynne <lewis.start0905@gmail.com>
2017-03-22 16:59:31 -0700

    add function to forms, styling for page

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-(document).ready(function() {
-  $(#formOne).submit(function(event) {
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var prodPick = $("#product").val();
+    var colorPick = $("#color").val();
+    var coolPick = $("#coolness").val();
+
+    $("#prodList").append("<li>"+prodPick+"</li>");
+    $("#colorList").append("<li>"+colorPick+"</li>");
+    $("#coolList").append("<li>"+coolPick+"</li>");
+
+
+    $("#receipt").show();
 
     event.preventDefault();
   });
-  $(#formTwo).submit(function(event) {
-
-    event.preventDefault();
-  });
-  $(#formThree).submit(function(event) {
-
-    event.preventDefault();
-  });
-
 });
